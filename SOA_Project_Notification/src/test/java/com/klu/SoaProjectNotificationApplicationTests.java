398280337430eda44382eb2e04ed69b4b8102aad
Gowrish2007 <2400033158@kluniversity.in>
2026-09-22 16:32:26 +0530

    updated the bugs

--- SOA_Project_Notification/src/test/java/com/klu/SoaProjectNotificationApplicationTests.java
+++ SOA_Project_Notification/src/test/java/com/klu/SoaProjectNotificationApplicationTests.java
@@ -2,6 +2,8 @@ package com.klu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+
 @SpringBootTest
 class SoaProjectNotificationApplicationTests {
 
